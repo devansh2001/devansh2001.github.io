477410d85d7ed14e746adc205fdef9b14d0c82b6
Devansh Ponda <dp185197@WUSDP185197-R95.com>
2019-06-11 15:18:22 -0400

    Added typing effect

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,16 @@
 import React, {Component} from 'react'
+import ReactTypingEffect from 'react-typing-effect'
 
 class Home extends Component {
   render() {
     return (
         <div id={'Home'} className={'main'}>
-          <h2 className={'name-intro'}>Hi! I am <strong>Devansh Jatin Ponda</strong></h2>
+          {/*<h2 className={'name-intro'}>Hi! I am <strong>{this.state.currComponent}</strong></h2>*/}
+          <h2 className={'name-intro-header'}>Hi! I am </h2>
+          <h2 style={{fontFamily: 'monospace'}} className={'name-intro'}><ReactTypingEffect
+                             speed={30}
+                             eraseDelay={1000}
+                             text={['Devansh Jatin Ponda', 'a Developer', 'a Researcher']}/></h2>
         </div>
     );
   }

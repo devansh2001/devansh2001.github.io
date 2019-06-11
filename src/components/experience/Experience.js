4e0652319c8f94215e21b8eb53f6e39fc27dfe66
Devansh Ponda <dp185197@WUSDP185197-R95.com>
2019-06-11 09:47:42 -0400

    Added JavaScript to change background based on hover

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -6,15 +6,30 @@ import Row from "react-bootstrap/Row";
 
 class Experience extends Component {
 
-  mouseHandle = () => {
+  mouseHandle = (event) => {
+    var num = event.target.className;
+    if (num.indexOf('koc') !== -1) {
+      console.log('KOC');
+      this.addStyle('https://cdn.pixabay.com/photo/2016/06/18/17/42/image-1465348_960_720.jpg');
+    } else if (num.indexOf('taing') !== -1) {
+      console.log('taing');
+    } else if (num.indexOf('ncr') !== -1) {
+      console.log('NCR');
+    } else {
+      console.log('Why did you fire?');
+    }
     console.log('Entered!');
-    document.getElementById('fancy-bg').style.background = "url('https://cdn.pixabay.com/photo/2016/06/18/17/42/image-1465348_960_720.jpg') no-repeat";
+  };
+
+  addStyle = (link) => {
+    document.getElementById('fancy-bg').style.background = "url(" + link + ")";
   };
 
   mouseLeave = () => {
     console.log('Left!');
     document.getElementById('fancy-bg').style.background = '#FEFEFE'
-  }
+  };
+
   render() {
     return (
         <div id={'Experience'} className={'main my-work-exp'}>
@@ -28,9 +43,9 @@ class Experience extends Component {
                       dateText="May '19 – Aug '19"
                       style={{ color: '#e86971' }}
                   >
-                    <h3>NCR Corporation</h3>
-                    <h4>Software Engineering Intern -- Cloud Platform</h4>
-                    <p on  className={'para-exp-data'}>
+                    <h3 className={'ncr'} onMouseEnter={this.mouseHandle} onMouseLeave={this.mouseLeave}>NCR Corporation</h3>
+                    <h4 className={'ncr'} onMouseEnter={this.mouseHandle} onMouseLeave={this.mouseLeave}>Software Engineering Intern -- Cloud Platform</h4>
+                    <p className={'para-exp-data ncr'} onMouseEnter={this.mouseHandle} onMouseLeave={this.mouseLeave}>
                       As a software engineering intern on the Cloud Platform team, my team and I
                       built an order monitoring tool that tracks the progress of all incoming orders
                       through the NCR system. We also developed a Kibana dashboard to visualize all
@@ -43,9 +58,9 @@ class Experience extends Component {
                       dateText="Jan '19 - May '19"
                       dateInnerStyle={{ background: '#61b8ff' }}
                   >
-                    <h3>Teaching Assistant</h3>
-                    <h4>Introduction to Object-Oriented Programming</h4>
-                    <p className={'para-exp-data'}>
+                    <h3 className={'taing'} onMouseEnter={this.mouseHandle} onMouseLeave={this.mouseLeave}>Teaching Assistant</h3>
+                    <h4 className={'taing'} onMouseEnter={this.mouseHandle} onMouseLeave={this.mouseLeave}>Introduction to Object-Oriented Programming</h4>
+                    <p className={'para-exp-data taing'} onMouseEnter={this.mouseHandle} onMouseLeave={this.mouseLeave}>
                       'The best way to learn is to teach.' I am a Teaching Assistant for the
                       'Introduction to Object-Oriented Programming' class at Georgia Tech. We see
                       over 800 students every semester, but my team and I ensure that everyone gets
@@ -55,15 +70,15 @@ class Experience extends Component {
                     </p>
                   </TimelineItem>
 
-                  <div onMouseEnter={this.mouseHandle} onMouseLeave={this.mouseLeave}>
+                  <div>
                     <TimelineItem
                         key="003"
                         dateText="Jun '17 – Jul '17"
                         dateInnerStyle={{ background: '#76bb7f' }}
                     >
-                      <h3>Kuwait Oil Company</h3>
-                      <h4>Information Technology Intern</h4>
-                      <p className={'para-exp-data'}>
+                      <h3 className={'koc'} onMouseEnter={this.mouseHandle} onMouseLeave={this.mouseLeave}>Kuwait Oil Company</h3>
+                      <h4 className={'koc'} onMouseEnter={this.mouseHandle} onMouseLeave={this.mouseLeave}>Information Technology Intern</h4>
+                      <p className={'para-exp-data koc'} onMouseEnter={this.mouseHandle} onMouseLeave={this.mouseLeave}>
                         Est incididunt sint eu minim dolore mollit velit velit commodo ex nulla
                         exercitation. Veniam velit adipisicing anim excepteur nostrud magna
                         nostrud aliqua dolor. Sunt aute est duis ut nulla officia irure
